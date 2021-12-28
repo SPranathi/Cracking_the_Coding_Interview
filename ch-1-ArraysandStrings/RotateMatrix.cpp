b6586df0cbf1f3fda29bce158d0c143b2e6f6802
Pranathi <sripadapranathi@gmail.com>
2021-12-28 23:44:07 +0530

    change

---
+++ ch-1-ArraysandStrings/RotateMatrix.cpp
@@ -0,0 +1,38 @@
+/*
+Chapter 01 - Problem 07 - Rotate Matrix - CTCI 6th Edition page 91
+
+Problem Statement:
+Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes,
+write a method to rotate the image by 90 degrees. Can you do this in place?
+
+Example:
+[1,2,3,       [7,4,1,
+4,5,6,   ->   8,5,2,
+7,8,9]        9,6,3]
+
+Time complexity: O(N^2). One operation per pixel in the image.
+
+Space complexity: O(1). We only need to allocate enough space for one matrix cell.
+*/ 
+#include<iostream>
+using namespace std;
+void rotate(int **arr,int n){
+    
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int **arr=new int*[n];
+    for(int i=0;i<n;i++){
+        arr[i]=new int[n];
+        for(int j=0;j<n;j++)
+            cin>>arr[i][j];
+    }
+    rotate(arr,n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++)
+            cout<<arr[i][j];
+    }
+
+}
