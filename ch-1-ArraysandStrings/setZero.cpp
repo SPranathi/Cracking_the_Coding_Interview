c4ed8590a8d68b62d61b24ed7315f040fa3cc774
Pranathi <sripadapranathi@gmail.com>
2021-12-31 09:47:43 +0530

    setZer0

--- ch-1-ArraysandStrings/setZero.cpp
+++ ch-1-ArraysandStrings/setZero.cpp
@@ -1,21 +1,87 @@
 /*
 Chapter 01 - Problem 08 - Set Zero - CTCI 6th Edition page 91
+
 Problem Statement:
 Write an algorithm such that if an element in an MxN matrix is 0, its entire
 row and column are set to 0.
+
 Example:
 [1, 2, 0, 4,      [0, 0, 0, 0,
  1, 2, 3, 4,  ->   0, 2, 0, 4,
  0, 2, 3, 4]       0, 0, 0, 0]
-Solution:
-We will implement this with allocating constant additional space. Use two boolean
-variables firstZeroRow and firstZeroCol that indicate whether the first row and column
-must be zeroed out. Search the first row and column for zeros to determine the value of
-these boolean values. Next, use the first row as storage to note which columns must be zeroed.
-Use the first column as storage to determine which rows must be zeroed. In Gayle's words
-"iterate through the rest of the matrix, setting matrix[i][0] and matrix[0][j] to zero
-whenever there's a zero in matrix[i][j]." Finally, iterate through the first row and column
-to zero out the rows and columns indicated.
+
 Time complexity: O(MxN).
+
 Space complexity: O(1).
 */ 
+#include<iostream>
+using namespace std;
+
+void setZero(int **arr,int m,int n){
+    //determine if first row and column contain zeros 
+    bool firstZeroRow=false;
+    bool firstZeroCol=false;
+    
+    for(int r=0;r<m;r++){
+        if(arr[r][0]==0){
+            firstZeroCol=true;
+            break;
+        }
+    }
+    for(int c=0;c<n;c++){
+        if(arr[0][c]==0){
+            firstZeroRow=true;
+            break;
+        }
+    }
+    //check the rest of the matrix for zeros and use frist row and col to
+    //store this information
+    for(int r=1;r<m;r++){
+        for(int c=1;c<n;c++){
+            if(arr[r][c]==0){
+                arr[0][c]=0;
+                arr[r][0]=0;
+            }
+        }
+    } 
+    
+    //look at storage and apply zeros to appropriate rows and columns
+    for(int r=1;r<m;r++){
+        if(arr[r][0]==0){
+            for(int c=1;c<n;c++)
+                arr[r][c]=0;
+        }
+    }
+    for(int c=1;c<n;c++){
+        if(arr[0][c]==0){
+            for(int r=1;r<m;r++)
+                arr[r][c]=0;
+        }
+    }
+    //look at first row and first col booleans to zero out first row and first col
+    if(firstZeroCol){
+        for(int r=0;r<m;r++)
+                arr[r][0]=0;
+    }
+    if(firstZeroRow){
+        for(int c=0;c<n;c++)
+                arr[0][c]=0;
+    }
+}
+
+int main(){
+    int m,n;
+    cin>>m>>n;
+    int **arr=new int*[m];
+    for(int i=0;i<m;i++){
+        arr[i]=new int[n];
+        for(int j=0;j<n;j++)
+            cin>>arr[i][j];
+    }
+    setZero(arr,m,n);
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++)
+            cout<<arr[i][j]<<" ";
+        cout<<endl;
+    }
+}
