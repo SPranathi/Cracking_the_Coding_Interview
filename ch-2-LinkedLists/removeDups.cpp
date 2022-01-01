166cca65f1e4ce841dba25b0f86ed739d4c32edc
Pranathi <sripadapranathi@gmail.com>
2022-01-01 07:14:53 +0530

    change

--- ch-2-LinkedLists/removeDups.cpp
+++ ch-2-LinkedLists/removeDups.cpp
@@ -1,1 +1,14 @@
- 
+/*
+PROBLEM:
+Write code to remove duplicates from an unsorted linked list.
+
+Example:
+1 1 2 3 4 4 
+
+TIME COMPLEXITY:
+O(n), because we iterate through the singly linked list only once, performing constant
+work over each element.
+
+SPACE COMPLEXITY:
+O(n), because in the worst case we store each element in the hashmap.
+*/ 
