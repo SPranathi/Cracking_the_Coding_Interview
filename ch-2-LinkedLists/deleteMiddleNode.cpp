a212e6fbec9a8cb7e5596768a2cb246bb9b291c5
Pranathi <sripadapranathi@gmail.com>
2022-01-16 17:16:01 +0530

    change

--- ch-2-LinkedLists/deleteMiddleNode.cpp
+++ ch-2-LinkedLists/deleteMiddleNode.cpp
@@ -1,1 +1,18 @@
+/*
+Problem Statement:
+Implement an algorithm to delete a node in the middle (i.e., any node but the first and last
+node, not necessarily the exact middle) of a singly linked list, given only access to that node.
+
+Example:
+Input: the node c from the linked list a -> b -> c -> d -> e -> f
+Result: nothing is returned, but the new linked list looks like a -> b -> d -> e -> f
+
+IMPLEMENTATION:
+Copy the data from the next node into the node of choice, then delete that node.
+
+TIME COMPLEXITY:
+O(1), as we perform 3 assignments and one deletion.
+
+SPACE COMPLEXITY:
+O(1), as we do not use any space for this algorithm.
  
